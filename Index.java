008e38f0a72dc54a83099140056502ed49188d2a
Michael Clermont <michael.clermont@mths.ca>
2022-10-12 03:03:02 +0000

    added maxrun program

---
+++ Index.java
@@ -0,0 +1,62 @@
+/*
+ *
+ * The program finds the maxium run of a string
+ *
+ * @author:  Michael Clermontl
+ * @version: 1.0
+ * @since:   2022-9-26
+*/
+
+import java.util.Scanner;
+
+/**
+* This is the starting of main program.
+*/
+public final class Index {
+    /**
+    * The utility class lint exception rule.
+    *
+    */
+    private Index() {
+        // nothing called
+    }
+
+    /**
+    * The main calculation function.
+    *
+    * @param userWord - width number
+    * @return maxRun - int
+    */
+
+    static int maxRunCalculator(final String userWord) {
+        int count = 0;
+        int maxRun = 0;
+        char blankChar = ' ';
+        for (char tempValue : userWord.toCharArray()) {
+            if (tempValue == blankChar) {
+                count++;
+            } else {
+                count = 1;
+                blankChar = tempValue;
+            }
+            if (count > maxRun) {
+                maxRun = count;
+            }
+        }
+        return maxRun;
+    }
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used.
+    */
+
+    public static void main(final String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        final String userString = sc.nextLine();
+        System.out.printf("\nThe max amount of values in a row is %d",
+                        maxRunCalculator(userString));
+        System.out.println("\nDone.");
+    }
+}
